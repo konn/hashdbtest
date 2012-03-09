8713d3b5515b8b68a359cb838d082ebf3d3ab378
Hiromi ISHII <hiromi@mail.konn-san.com>
2012-03-09 16:09:27 +0900

    status condition

--- redirector.hs
+++ redirector.hs
@@ -3,11 +3,16 @@ module Main where
 import Network.Wai
 import Network.Wai.Handler.Warp
 import qualified Network.HTTP.Types as H
+import Control.Monad.IO.Class
 
 main :: IO ()
 main = run 8910 application
 
 application :: Application
 application req@Request {pathInfo=["test"]} = do
-  return $ responseLBS H.status303 [("location", "http://konn-san.com/test2/")] "moved"
+  liftIO $ print $ httpVersion req
+  let status = if httpVersion req >= H.http11
+               then H.status303
+               else H.status302
+  return $ responseLBS status [("location", "http://konn-san.com/test2/")] "moved"
 application _ = return $ responseLBS H.status200 [] "it works!"
