97d6d4e58cb8d6438b00f3c5a22d5f2ff3605d3c
Hiromi Ishii <konn.jinro@gmail.com>
2012-03-24 15:24:05 +0900

    oops

--- redirector.hs
+++ redirector.hs
@@ -3,11 +3,19 @@ module Main where
 import Network.Wai
 import Network.Wai.Handler.Warp
 import qualified Network.HTTP.Types as H
+import Control.Monad.IO.Class
 
 main :: IO ()
-main = run 89104 application
+main = run 8910 application
 
 application :: Application
 application req@Request {pathInfo=["test"]} = do
-  return $ responseLBS H.status303 [("location", "http://konn-san.com/test2/")] "moved"
+  liftIO $ print $ httpVersion req
+  let status = if httpVersion req >= H.http11
+               then H.status303
+               else H.status302
+  return $ responseLBS status [("location", "/redirected")] "moved"
+application req@Request {pathInfo=["redirected"]} = do
+  return $ responseLBS H.status200 [] "hey, you're redirected!"
 application _ = return $ responseLBS H.status200 [] "it works!"
+
